b8589d9f1fe40ab4d8448fa7335359a9ca1d6445
rrishabhrg <rishabh.gupta@successive.tech>
2019-06-10 16:48:06 +0530

    feat: add features

--- src/pages/Trainee/Components/EditIcon/UpdateIcon.jsx
+++ src/pages/Trainee/Components/EditIcon/UpdateIcon.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable array-callback-return */
+/* eslint-disable arrow-parens */
 /* eslint-disable no-console */
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
@@ -12,9 +14,15 @@ import TextField from '@material-ui/core/TextField';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import AccountCircle from '@material-ui/icons/AccountCircle';
 import Email from '@material-ui/icons/Email';
+import schema from './Schema';
 
 class UpdateIcon extends React.Component {
   state = {
+    name: '',
+    emailAddress: '',
+    Errors: {},
+    isTouch: [],
+    btnDisabled: true,
     open: false,
   }
 
@@ -34,8 +42,85 @@ class UpdateIcon extends React.Component {
     });
   }
 
+  handleNameChange = (event) => {
+    const { name } = this.state;
+    this.setState({
+      name: event.target.value,
+    }, this.validator);
+    console.log('value of NAME is: ', name);
+  }
+
+  handleEmailChange = (event) => {
+    const { emailAddress } = this.state;
+    this.setState({
+      emailAddress: event.target.value,
+    }, this.validator);
+    console.log('value of EMAIL is: ', emailAddress);
+  }
+
+  handleNameTouch = () => {
+    const { isTouch } = this.state;
+    if (!isTouch.includes('name')) {
+      isTouch.push('name');
+    }
+    this.setState({
+      isTouch,
+    }, this.validator);
+  }
+
+  handleEmailTouch = () => {
+    const { isTouch } = this.state;
+    if (!isTouch.includes('emailAddress')) {
+      isTouch.push('emailAddress');
+    }
+    this.setState({
+      isTouch,
+    }, this.validator);
+  }
+
+  hasErrors = (value) => {
+    const { Errors } = this.state;
+    return Errors[value] ? Errors[value] : '';
+  }
+
+  isTouch = (value) => {
+    const { isTouch } = this.state;
+    return isTouch.includes(value);
+  }
+
+  getErrors = (value) => {
+    const { Errors } = this.state;
+    if (this.isTouch(value) && this.hasErrors(value)) {
+      return Errors[value];
+    }
+    return '';
+  }
+
+  validator = () => {
+    const {
+      name, emailAddress,
+    } = this.state;
+    schema.validate({
+      name, emailAddress,
+    }, { abortEarly: false }).then(() => {
+      this.setState({
+        Errors: {},
+        btnDisabled: false,
+      });
+    }).catch((errors) => {
+      const error = {};
+      errors.inner.map(err => {
+        error[err.path] = err.message;
+      });
+      this.setState({
+        Errors: error,
+        btnDisabled: true,
+      });
+    });
+  }
+
   render() {
-    const { open } = this.state;
+    const { open, btnDisabled } = this.state;
     return (
       <React.Fragment>
         <Button onClick={this.handleEditDialogClickOpen}>
@@ -52,12 +137,14 @@ class UpdateIcon extends React.Component {
                 autoFocus
                 fullWidth
                 required
-                id="email"
-                name="name"
+                id="outlined-required"
                 label="Name"
-                variant="outlined"
                 margin="normal"
-                autoComplete="name"
+                variant="outlined"
+                onChange={this.handleNameChange}
+                onBlur={this.handleNameTouch}
+                error={!!this.getErrors('name')}
+                helperText={this.getErrors('name')}
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
@@ -69,12 +156,15 @@ class UpdateIcon extends React.Component {
               <TextField
                 fullWidth
                 required
-                id="email"
-                name="email"
+                id="outlined-required"
                 label="Email Address"
                 type="email"
-                variant="outlined"
                 margin="normal"
+                variant="outlined"
+                onChange={this.handleEmailChange}
+                onBlur={this.handleEmailTouch}
+                error={!!this.getErrors('emailAddress')}
+                helperText={this.getErrors('emailAddress')}
                 InputProps={{
                   startAdornment: (
                     <InputAdornment position="start">
@@ -87,7 +177,7 @@ class UpdateIcon extends React.Component {
           </DialogContent>
           <DialogActions>
             <Button onClick={this.handleEditDialogClickClose} color="primary">Cancel</Button>
-            <Button disabled variant="contained" color="primary">Submit</Button>
+            <Button variant="contained" color="primary" disabled={btnDisabled}>Submit</Button>
           </DialogActions>
         </Dialog>
       </React.Fragment>
