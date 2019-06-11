5f470f720c5748993aa5e770c93bf5f4c2b3514f
rrishabhrg <rishabh.gupta@successive.tech>
2019-06-11 17:37:33 +0530

    fix: changes

---
+++ src/Contexts/SnackBarProvider/SnackBarProvider.jsx
@@ -0,0 +1,14 @@
+/* eslint-disable react/prefer-stateless-function */
+import React from 'react';
+
+class SnackBarProvider extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+
+      </React.Fragment>
+    );
+  }
+}
+
+export default SnackBarProvider;
